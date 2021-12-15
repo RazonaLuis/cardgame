4afa8e1308e441d643c2f34c01c7e5b7368ca40d
lheshiki <l.heshiki@razona.jp>
2021-12-15 18:23:38 +0900

    [fix]パスの修正

--- assets/js/cats.js
+++ assets/js/cats.js
@@ -1,42 +1,42 @@
 const cats = [
   {
     name: 'アメリカンカール',
-    image: 'assets/img/americancurl.png',
+    image: './assets/img/americancurl.png',
   },
   {
     name: 'サイベリアン',
-    image: 'assets/img/siberian.png',
+    image: './assets/img/siberian.png',
   },
   {
     name: 'シャム',
-    image: 'assets/img/siamese.png',
+    image: './assets/img/siamese.png',
   },
   {
     name: 'スコティッシュフォールド',
-    image: 'assets/img/scottishfold.png',
+    image: './assets/img/scottishfold.png',
   },
   {
     name: 'スフィンクス',
-    image: 'assets/img/sphynx.png',
+    image: './assets/img/sphynx.png',
   },
   {
     name: 'メインクーン',
-    image: 'assets/img/mainecoon.png',
+    image: './assets/img/mainecoon.png',
   },
   {
     name: 'ロシアンブルー',
-    image: 'assets/img/russianblue.png',
+    image: './assets/img/russianblue.png',
   },
   {
     name: 'ラガマフィン',
-    image: 'assets/img/ragamuffin.png',
+    image: './assets/img/ragamuffin.png',
   },
   {
     name: 'マンチカン',
-    image: 'assets/img/munchkin.png',
+    image: './assets/img/munchkin.png',
   },
   {
     name: 'キンカロー',
-    image: 'assets/img/kinkalow.png',
+    image: './assets/img/kinkalow.png',
   },
 ];
